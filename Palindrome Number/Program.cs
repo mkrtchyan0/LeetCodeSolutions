30b231ca271273ce9cfd0240ed23796399ed2e3d
Vardan <168998116+mkrtchyan0@users.noreply.github.com>
2025-04-09 10:36:36 +0400

    Easy

---
+++ Palindrome Number/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Palindrome_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool x = IsPalindrome(91919);
+            Console.WriteLine(x);
+        }
+        public static bool IsPalindrome(int x)
+        {
+            string num = x.ToString();
+            var reversed = new System.Text.StringBuilder();
+
+            for (int i = num.Length - 1; i >= 0; i--)
+            {
+                reversed.Append(num[i]);
+            }
+
+            return num == reversed.ToString();
+        }
+    }
+}
