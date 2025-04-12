4a0857a9ab416fd8656ccfa670f3228ff8cd407e
Vardan <168998116+mkrtchyan0@users.noreply.github.com>
2025-04-12 21:48:33 +0400

    Medium
    Given a string s, return the longest palindromic substring in s.

---
+++ Longest Palindromic Substring/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Longest_Palindromic_Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var res = LongestPalindrome("babad");
+            Console.WriteLine(res);
+        }
+        public static string LongestPalindrome(string s)
+        {
+            int count = s.Length;
+            int currentCount = 1;
+            string result = "";
+            int b = 0;
+
+            if (s.Length == 1)
+            {
+                return s;
+            }
+            List<char> charikner1 = new List<char>();
+            List<char> charikner2 = new List<char>();
+            while (currentCount != count)
+            {
+                for (int k = b; k <= currentCount; k++)
+                {
+                    charikner1.Add(s[k]);
+                }
+                for (int k = currentCount; k >= b; k--)
+                {
+                    charikner2.Add(s[k]);
+                }
+                b++;
+
+                string left = new string(charikner1.ToArray());
+                string right = new string(charikner2.ToArray());
+
+                if (left == right)
+                {
+                    if (result.Length < left.Length)
+                    {
+                        result = left;
+                    }
+
+                    currentCount++;
+                    b = 0;                    
+                }
+                if (b == count)
+                {
+                    currentCount++;
+                    b = 0;
+                }
+                charikner1.Clear();
+                charikner2.Clear();
+            }
+            return result;
+        }
+    }
+}
