e542a9263ece95a3846bf235a22d940c67fde4fe
Vardan <168998116+mkrtchyan0@users.noreply.github.com>
2025-06-20 13:19:06 +0400

    Add files via upload

---
+++ Valid Parentheses/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Valid_Parentheses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "({{{{}}}))";
+            var res = IsValid(s);
+
+            Console.WriteLine(res);
+        }
+        public static bool IsValid(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char c in s)
+            {
+                if (c == '(' || c == '{' || c == '[')
+                {
+                    stack.Push(c);
+                }
+                else
+                {
+                    if (stack.Count == 0)
+                        return false;
+
+                    char top = stack.Pop();
+
+                    if ((c == ')' && top != '(') ||
+                        (c == '}' && top != '{') ||
+                        (c == ']' && top != '['))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return stack.Count == 0;
+        }
+    }
+}
