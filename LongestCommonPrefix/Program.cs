1f29e64419d8266ee892757f46367fa76762dd18
Vardan <168998116+mkrtchyan0@users.noreply.github.com>
2025-08-07 16:49:48 +0400

    LongestCommonPrefix
    Medium

---
+++ LongestCommonPrefix/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace LongestCommonPrefix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strs = { "", "b" };
+            Console.WriteLine(LongestCommonPrefix(strs));
+        }
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            if (strs == null)
+            {
+                return "";
+            }
+            if (strs.Length == 1)
+            {
+                return strs[0];
+            }
+            string result = "";
+            int current = 0;
+
+            string firstWord = strs[0];
+            while (current < firstWord.Length)
+            {
+                for (int j = 1; j < strs.Length; j++)
+                {
+                    if (current >= strs[j].Length || firstWord[current] != strs[j][current])
+                    {
+                        return result;
+                    }
+                }
+                result += firstWord[current];
+                current++;
+            }
+            return result;
+        }
+    }
+}
